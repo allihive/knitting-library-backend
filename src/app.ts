212414796b615114782ac1db03588a6413443aa8
Alice Li <aliceli.bls@gmail.com>
2026-08-22 10:12:26 +0300

    backend and front end connected

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import authRoutes from './modules/auth/auth.route.js'
+import cors from "cors"
 
 
 const app = express();
 
+app.use((cors({
+	origin: "http://localhost:5173",
+	credentials: true,
+})))
+
 app.use(express.json()); //parses incoming JSON request bodies
 app.use('/auth', authRoutes);
+
+app.listen(5001, () => console.log("Server running on port 5001"))
 
 export default app;
