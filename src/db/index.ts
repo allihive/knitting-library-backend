bab28dae693ce63d72dcaf1334442ce85aaeb3f7
Alice Li <aliceli.bls@gmail.com>
2026-09-06 10:17:48 +0300

    ssl for aws rds database

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,6 +5,7 @@ import 'dotenv/config'
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {rejectUnauthorized: false}
 });
 
 export const db = drizzle(pool, { schema });
