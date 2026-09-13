8751863eeb1d11af087bf03295529ff5c08f6031
Alice Li <aliceli.bls@gmail.com>
2026-09-13 13:38:51 +0300

    rate limit added

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,26 @@
 import express from 'express';
 import authRoutes from './modules/auth/auth.route.js'
 import cors from "cors"
+import rateLimit from 'express-rate-limit';
 
 
 const app = express();
 
 app.use((cors({
-	origin: "http://localhost:5173",
+	origin: "https://aliceli.dev",
 	credentials: true,
 })))
 
 app.use(express.json()); //parses incoming JSON request bodies
-app.use('/auth', authRoutes);
 
-app.listen(5001, () => console.log("Server running on port 5001"))
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 20,
+  message: 'Too many attempts, please try again later.'
+});
+
+
+app.use('/auth', authLimiter, authRoutes);
+
 
 export default app;
