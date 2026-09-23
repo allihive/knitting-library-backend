cbfe9f5b64a3f9c78da22ae8618c9cf64576ef70
Alice Li <aliceli.bls@gmail.com>
2026-09-23 09:48:13 +0300

    added not found error

--- src/modules/middleware/errorHandler.ts
+++ src/modules/middleware/errorHandler.ts
@@ -1,10 +1,15 @@
 import type { Request, Response, NextFunction } from "express";
 import { ZodError } from 'zod';
+import { NotFoundError } from "../shared/errors";
 
 export function errorHandler(err: unknown, req: Request, res: Response, next: NextFunction): void{
 	console.error(err);
 	if (err instanceof ZodError) {
 		res.status(400).json({ error: err.issues });
+		return;
+	}
+	if (err instanceof NotFoundError) {
+		res.status(404).json({error: err.message});
 		return;
 	}
 
