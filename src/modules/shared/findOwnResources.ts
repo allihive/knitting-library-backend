4eb36e3785bbc472fbe1873c92356d55512bebfe
Alice Li <aliceli.bls@gmail.com>
2026-09-26 09:09:12 +0300

    find own resources completed

--- src/modules/shared/findOwnResources.ts
+++ src/modules/shared/findOwnResources.ts
@@ -3,7 +3,17 @@ import type { PgTableWithColumns, TableConfig} from 'drizzle-orm/pg-core'
 import { db } from '../../db/index'
 import { NotFoundError } from './errors'
 
-export async function findOwnedResource<T extends TableConfig>(
-	table: PgTableWithColumns<T> & { id: any, userId: any };
-	resource
-)
+export async function findOwnedResource<T extends TableConfig>( //able to return many types
+	table: PgTableWithColumns<T> & { id: any, userId: any },
+	resourceId: string,
+	userId: string,
+) {
+	const [row] = await db
+		.select()
+		.from(table as any)
+		.where(and(eq((table as any).id, resourceId), eq((table as any).userId,userId)));
+	if (!row) {
+		throw new NotFoundError();
+	}
+	return row;
+}
