53da266ac3836b4704f663cf6075d8a1744ac0bc
Alice Li <aliceli.bls@gmail.com>
2026-09-26 09:18:53 +0300

    get yarn and get all yarn added

--- src/modules/yarn/yarn.controller.ts
+++ src/modules/yarn/yarn.controller.ts
@@ -2,6 +2,8 @@ import { createYarnSchema } from "../yarn/yarn.validation";
 import { db } from '../../db/index.js'
 import { yarn } from '../../db/schema'
 import type { Request, Response } from 'express';
+import { findOwnedResource } from "../shared/findOwnResources";
+import { eq } from 'drizzle-orm'
 
 export async function createYarn(req: Request, res:Response): Promise<void> {
 	if (!req.user) {
@@ -15,4 +17,32 @@ export async function createYarn(req: Request, res:Response): Promise<void> {
 	userId: req.user.userId,
 }).returning();
 	res.status(201).json(newYarn);
+}
+
+export async function getYarn(req: Request, res: Response): Promise<void> {
+		if (!req.user) {
+		res.status(401).json({error: 'Unauthorized'});
+		return;
+	}
+	const { id } = req.params;
+
+	if (!id || typeof id !== 'string') {
+		res.status(400).json({error: 'Yarn id is required'});
+		return;
+	}
+	const yarnItem = await findOwnedResource(yarn, id, req.user.userId);
+	res.status(200).json(yarnItem);
+	return;
+}
+
+export async function getAllYarn(req: Request, res: Response): Promise<void> {
+	if (!req.user) {
+		res.status(401).json({error: 'Unauthorized'});
+		return;
+	}
+	const yarnItems = await db
+		.select()
+		.from(yarn)
+		.where(eq(yarn.userId, req.user.userId))
+	res.status(200).json(yarnItems);
 }
